f7be94ef3cb2575344d816d76411870cb335f803
Kränzlein Christoph <c.kraenzlein@campus.lmu.de>
2025-05-30 02:08:17 +0200

    Addition of a Training and Test Data Set with Installer formatted

--- CreateTestDataSet.r
+++ CreateTestDataSet.r
@@ -4,16 +4,13 @@ library(dplyr)
 library(here)
 # Set the working directory to the location of the script
 here::here()
-# Read in the Datasets
-TrainingData_Variables <- read_csv("TrainingData_Variables.csv")
-TrainingData_TargetVariable <- read_csv("TrainingData_TargetVariable.csv")
-# Combine the two datasets by the column "id"
-TrainingData <- merge(x = TrainingData_Variables, y = TrainingData_TargetVariable, by = "id")
+# Load the test data
+TestData <- readr::read_csv("TestData.csv")
 
 # Remov some variables that are not needed for the analysis and format the data, order some factors
-TrainingData_factor_ordered <- TrainingData %>% 
+TestData_factor_ordered <- TestData %>% 
     select(amount_tsh, gps_height, longitude, latitude, region, population, construction_year, extraction_type,
-           management_group, payment, quality_group, quantity, source_type, source_class, waterpoint_type, installer, extraction_type_class, status_group) %>%
+           management_group, payment, quality_group, quantity, source_type, source_class, waterpoint_type, installer, extraction_type_class) %>%
     mutate(waterpoint_type = as.factor(waterpoint_type), # amount of water available
            source_type = as.factor(source_type), # The source of the water, since the variable wasnt that important in the first model, 
                                        # we use source_type because it has less levels
@@ -29,27 +26,22 @@ TrainingData_factor_ordered <- TrainingData %>%
            extraction_type = as.factor(extraction_type), # The kind of extraction method used
            extraction_type_class = as.factor(extraction_type_class), # The kind of extraction method used, fewer levels than extraction_type
            region = as.factor(region), # 21 regions: islands and some small regions (fewer residents) are not included
-           installer = as.factor(installer), # Organization that installed the well
-           status_group = as.factor(status_group) # The target variable
-           )
+           installer = as.factor(installer))
 
 # Replace NA values in installer with "unknown"
-TrainingData_factor_ordered$installer <- as.character(TrainingData_factor_ordered$installer)
-TrainingData_factor_ordered$installer[is.na(TrainingData_factor_ordered$installer)] <- "unknown"
+TestData_factor_ordered$installer <- as.character(TestData_factor_ordered$installer)
+TestData_factor_ordered$installer[is.na(TestData_factor_ordered$installer)] <- "unknown"
 # Convert installer back to factor
-TrainingData_factor_ordered$installer <- as.factor(TrainingData_factor_ordered$installer)
+TestData_factor_ordered$installer <- as.factor(TestData_factor_ordered$installer)
 
-# Format installer by the number of instlled waterpoints
+Id <- TestData$id
 
-Top25_installers <- TrainingData_factor_ordered %>%
-    select(installer) %>%
-    group_by(installer) %>%
-    summarise(count = n()) %>%
-    arrange(desc(count)) %>%
-    head(25) %>%
-    ungroup()
 
-TrainingData_FormattedInstaller <- TrainingData_factor_ordered %>%
+
+# Formatted Installer for Test Data
+source("CreateTrainingData.r")
+
+TestData_FormattedInstaller <- TestData_factor_ordered %>%
     mutate(installer = as.character(installer)) %>%
     mutate(installer = ifelse(installer %in% Top25_installers$installer, installer, "other")) %>%
     mutate(installer = as.factor(installer))
