737465b8c839a6a3f95b5f382da44e062bb9d621
Kränzlein Christoph <c.kraenzlein@campus.lmu.de>
2025-08-16 17:43:21 +0200

    Update Features

--- CreateTestDataSet.r
+++ CreateTestDataSet.r
@@ -25,13 +25,23 @@ TestData_Formatted <- TestData %>%
            public_meeting = ifelse(public_meeting, "yes", ifelse(!public_meeting, "no", "unknown")), # Convert public_meeting to factor with levels yes, no, unknown
            public_meeting = ifelse(is.na(public_meeting), "unknown", public_meeting), # Replace NA values in public_meeting with "unknown"
            construction_year = ifelse(construction_year == 0, median_construction_year, construction_year), # Replace missing values in construction_year with the median of the known values
+           gps_height = ifelse(gps_height == 0, median(gps_height[gps_height > 0], na.rm = TRUE), gps_height), # Replace missing values in gps_height with the median of the known values
            mean_longitude = ifelse(longitude != 0, longitude, mean_longitude), # Replace missing values in longitude with the mean of the known values
            mean_latitude = ifelse(latitude != 2e-8, latitude, mean_latitude), # Replace missing values in latitude with the mean of the known values
            installer = as.character(installer)) %>% # Convert installer to character
     mutate(installer = ifelse(installer %in% Top25_installers$installer, installer, "other")) %>% # Replace installers not in the top 25 with "other"
     select(-longitude, -latitude) %>% # Remove longitude and latitude, because they are replaced by mean_longitude and mean_latitude
     mutate(payment_type = factor(payment_type, ordered = TRUE, levels = c("per bucket", "monthly", "annually", "on failure", "other", "unknown", "never pay")), # Convert payment_type to factor with ordered levels
+           quantity = factor(quantity, ordered = TRUE, levels = c("dry", "insufficient", "seasonal", "enough", "unknown")), # Convert quantity to factor with ordered levels
            permit = factor(permit), # Convert permit to factor with ordered levels
            public_meeting = factor(public_meeting), # Convert public_meeting to factor with ordered levels
            installer = as.factor(installer), # Convert installer to factor
-           district_code = as.factor(district_code)) # Convert district_code to factor
+           district_code = as.factor(district_code)) %>% # Convert district_code to factor
+    mutate(installer = ifelse(is.na(installer), "other", installer)) %>%
+    mutate(installer = as.factor(installer)) %>%
+    mutate(Season_recorded = case_when(
+        month_recorded %in% c(1, 2) ~ "dry",
+        month_recorded %in% c(3, 4, 5) ~ "rainy",
+        month_recorded %in% c(6, 7, 8, 9, 10) ~ "dry",
+        month_recorded %in% c(11, 12) ~ "rainy",
+    ))
