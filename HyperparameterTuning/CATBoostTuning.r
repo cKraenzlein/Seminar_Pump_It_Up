430b1e388e4509761c6884f42852d5d3ca545823
cKraenzlein <120517943+cKraenzlein@users.noreply.github.com>
2025-08-24 15:46:33 +0200

    update catboost

--- HyperparameterTuning/CATBoostTuning.r
+++ HyperparameterTuning/CATBoostTuning.r
@@ -20,18 +20,17 @@ set.seed(24)
 RF_imputation_regr = lrn("regr.ranger",num.threads = 8, num.trees = 250,min.node.size = 5,mtry = 6,max.depth=10)
 RF_imputation_classif = lrn("classif.ranger",num.threads = 8, num.trees = 250,min.node.size = 5,mtry = 6,max.depth=10)
 
-flag_missing = po("missind")
+flag_missing = po("missind", affect_columns = selector_type(c("numeric", "logical", "factor")))
 imp_num = po("imputelearner", learner = RF_imputation_regr, affect_columns = selector_name(c("longitude", "latitude")), id = "impute_num")
 imp_factor = po("imputelearner", learner = RF_imputation_classif, affect_columns = selector_type("factor"), id = "impute_factor")
 imp_bin = po("imputelearner", learner = RF_imputation_classif, affect_columns = selector_type("logical"), id = "impute_bin")
 po_select = po("select", selector = selector_invert(selector_name(c("missing_population_log", "missing_gps_height", "missing_longitude", "missing_construction_year", "missing_amount_tsh_log"))))
 
-imp_sel <- flag_missing %>>% list(imp_num, imp_factor, imp_bin) %>>% po("featureunion") %>>% po_select
+imp_sel <- gunion(list(flag_missing, imp_num, imp_factor, imp_bin)) %>>% po("featureunion") %>>% po_select
 
-encode_factors = po("encode", id = "encode_factors", method = "one-hot", affect_columns = selector_type("factor"))
-logical_to_int = po("colapply", id = "encode_logical", applicator = as.integer, affect_columns = selector_type("logical"))
+logical_to_int = po("colapply", id = "encode_logical", applicator = as.numeric, affect_columns = selector_type("logical"))
 
-graph = imp_sel %>>% encode_factors %>>% logical_to_int
+graph = imp_sel  %>>% logical_to_int
 
 graph$plot()
 
@@ -46,23 +45,23 @@ learner = lrn("classif.catboost", id = "catboost", thread_count = 8)
 
 # Combine graph and learner
 graph_learner = as_learner(graph %>>% learner)
-View(graph_learner$param_set$data)
+
 # Hyperparameter space
 hyper_param_space <- ps(
-  catboost.iterations    = p_int(100, 1500),
-  ccatboost.learning_rate = p_dbl(0.01, 0.3),
+  catboost.iterations    = p_int(100, 2000),
+  catboost.learning_rate = p_dbl(0.01, 0.3),
   catboost.depth         = p_int(4, 10),
   catboost.l2_leaf_reg   = p_dbl(1, 10)
 )
 
 # Resampling and Measure Strategy
-resampling <- rsmp("cv", folds = 5)
+resampling <- rsmp("cv", folds = 3)
 measure    <- msr("classif.acc")
 
 # Define Tuner and Termination
 tuner_rs <- tnr("random_search")
 term_rs  <- trm("combo", list(trm("clock_time", stop_time = Sys.time() + 12 * 3600),
-                              trm("evals", n_evals = 2)), any = TRUE)
+                              trm("evals", n_evals = 100)), any = TRUE)
 
 # Tuning instance
 tuning_instance <- ti(
