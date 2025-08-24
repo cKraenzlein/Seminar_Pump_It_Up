94f3500033e4624f386892b8c817f6e7c88129e0
Kränzlein Christoph <c.kraenzlein@campus.lmu.de>
2025-08-24 16:22:40 +0200

    xgboost update to mbo

--- HyperparameterTuning/XGBoostTuning.r
+++ HyperparameterTuning/XGBoostTuning.r
@@ -4,6 +4,7 @@ library(mlr3viz)
 library(mlr3tuning) 
 library(mlr3extralearners)
 library(mlr3pipelines)
+library(mlr3mbo)
 
 # Set the working directory
 here::here()
@@ -19,13 +20,13 @@ set.seed(24)
 RF_imputation_regr = lrn("regr.ranger",num.threads = 8, num.trees = 250,min.node.size = 5,mtry = 6)
 RF_imputation_classif = lrn("classif.ranger",num.threads = 8, num.trees = 250,min.node.size = 5,mtry = 6)
 
-flag_missing = po("missind")
+flag_missing = po("missind", affect_columns = selector_type(c("numeric", "integer", "logical", "factor")))
 imp_num = po("imputelearner", learner = RF_imputation_regr, affect_columns = selector_name(c("longitude", "latitude")), id = "impute_num")
 imp_factor = po("imputelearner", learner = RF_imputation_classif, affect_columns = selector_type("factor"), id = "impute_factor")
 imp_bin = po("imputelearner", learner = RF_imputation_classif, affect_columns = selector_type("logical"), id = "impute_bin")
 po_select = po("select", selector = selector_invert(selector_name(c("missing_population_log", "missing_gps_height", "missing_longitude", "missing_construction_year", "missing_amount_tsh_log"))))
 
-imp_sel <- flag_missing %>>% list(imp_num, imp_factor, imp_bin) %>>% po("featureunion") %>>% po_select
+imp_sel <- gunion(list(flag_missing, imp_num, imp_factor, imp_bin)) %>>% po("featureunion") %>>% po_select
 
 encode_factors = po("encode", id = "encode_factors", method = "one-hot", affect_columns = selector_type("factor"))
 logical_to_int = po("colapply", id = "encode_logical", applicator = as.integer, affect_columns = selector_type("logical"))
